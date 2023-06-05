a0c5b9a9c8f20276df9844777537eb6280ffdf43
SadrilAmin1999 <sadrilshuvo420@gmail.com>
2023-06-05 17:30:54 +0600

    slider done

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -1,8 +1,9 @@
 "use client";
 import { Swiper, SwiperSlide } from "swiper/react";
-import { Navigation } from "swiper";
+import { Navigation, Pagination, Autoplay, Parallax } from "swiper";
 import "swiper/css";
 import "swiper/css/navigation";
+import "swiper/css/pagination";
 import { data } from "@/data/sliderimages";
 import Image from "next/image";
 import Link from "next/link";
@@ -12,8 +13,17 @@ const Hero = () => {
   return (
     <section className="w-full h-[75vh]">
       <Swiper
+        parallax={true}
+        grabCursor={true}
+        autoplay={{
+          delay: 3000,
+          disableOnInteraction: false,
+        }}
+        loop={true}
+        speed={700}
+        pagination={{ clickable: true }}
         navigation={true}
-        modules={[Navigation]}
+        modules={[Navigation, Pagination, Autoplay, Parallax]}
         className="mySwiper w-full h-full"
       >
         {data.map((item) => (
@@ -29,11 +39,20 @@ const Hero = () => {
               />
               <Overlay />
               <div className=" absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 text-white z-[2] text-center space-y-5">
-                <h1 className="text-8xl font-semibold">{item.heading}</h1>
-                <p className=" text-lg">{item.paragraph}</p>
-                <Link className="btn btn-accent" href={"/products"}>
-                  {item.cta}
-                </Link>
+                <h1
+                  data-swiper-parallax="-400%"
+                  className="text-8xl font-semibold"
+                >
+                  {item.heading}
+                </h1>
+                <p data-swiper-parallax="-500%" className=" text-lg">
+                  {item.paragraph}
+                </p>
+                <div data-swiper-parallax="-600%">
+                  <Link className="btn btn-accent" href={"/products"}>
+                    {item.cta}
+                  </Link>
+                </div>
               </div>
             </div>
           </SwiperSlide>
