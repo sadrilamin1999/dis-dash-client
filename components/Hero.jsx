863b6e29e10d91ceb404e5b13b3d119495c073d7
SadrilAmin1999 <sadrilshuvo420@gmail.com>
2023-06-05 16:47:01 +0600

    overlay added in slider

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -4,21 +4,37 @@ import { Navigation } from "swiper";
 import "swiper/css";
 import "swiper/css/navigation";
 import { data } from "@/data/sliderimages";
+import Image from "next/image";
+import Link from "next/link";
+import Overlay from "./Overlay";
 
 const Hero = () => {
   return (
-    <section>
-      <Swiper navigation={true} modules={[Navigation]} className="mySwiper">
+    <section className="w-full h-[75vh]">
+      <Swiper
+        navigation={true}
+        modules={[Navigation]}
+        className="mySwiper w-full h-full"
+      >
         {data.map((item) => (
           <SwiperSlide key={item.id}>
-            <div>
-              <image
+            <div className="w-full h-full relative">
+              <Image
                 src={item.src}
                 alt={item.alt}
                 width={1920}
                 height={1080}
                 priority
+                className="w-full h-full object-cover"
               />
+              <Overlay />
+              <div className=" absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 text-white z-[2] text-center space-y-5">
+                <h1 className="text-8xl font-semibold">{item.heading}</h1>
+                <p className=" text-lg">{item.paragraph}</p>
+                <Link className="btn btn-accent" href={"/products"}>
+                  {item.cta}
+                </Link>
+              </div>
             </div>
           </SwiperSlide>
         ))}
